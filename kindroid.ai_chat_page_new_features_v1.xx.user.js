fc2f66013c8e4ab80ebba6f59ba9b41075711706
BreatFR <6360676+breatfr@users.noreply.github.com>
2024-02-06 18:51:33 +0100

    Update kindroid.ai_chat_page_new_features_v1.xx.user.js

--- kindroid.ai_chat_page_new_features_v1.xx.user.js
+++ kindroid.ai_chat_page_new_features_v1.xx.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
-// @name        Kindroid - Chat page new features v1.xx
+// @name        Kindroid - test
 // @description New features for Kindroid's chat page
 // @namespace   https://kindroid.ai/home
 // @match       https://kindroid.ai/home
-// @updateURL   https://raw.githubusercontent.com/breatfr/Kindroid/main/kindroid.ai_chat_page_new_features_v1.xx.user.js
-// @version     1.02
+// @version     1.03
 // @author      BreatFR
 // @icon        https://play-lh.googleusercontent.com/plaMooDMA0URmZGnfDJmn-v4DEP1SGMUXxC0grATSPuXQ8-HgAgijTXVvyU1y1ir_fRd=w240-h480-rw
 // @license     GNU GPL v3 (https://www.gnu.org/licenses/quick-guide-gplv3.en.html)
@@ -84,9 +83,20 @@
             return;
         }
 
-        const confirmButton = document.querySelector('button.chakra-button .chakra-text');
-        if (confirmButton && confirmButton.textContent === 'Confirm') {
-            confirmButton.click();
+        // Cherchez le bouton "Confirm" dans le message de régénération
+        const confirmButton1 = document.querySelector('.css-mqmatd .chakra-text');
+        const confirmButton2 = document.querySelector('.css-1jbciej'); // Ajout du sélecteur pour le bouton Confirm
+
+        // Vérifiez d'abord si le texte spécifique est présent avant de sélectionner le bouton Confirm
+        const confirmationText = document.querySelector('.css-10813k0');
+        if (confirmationText && confirmationText.textContent.includes("Are you sure you want to regenerate this message?")) {
+            if (confirmButton1 && confirmButton1.textContent === 'Confirm') {
+                confirmButton1.click();
+            } else if (confirmButton2) { // Utilisation du deuxième sélecteur si le premier n'est pas trouvé
+                confirmButton2.click();
+            } else {
+                setTimeout(autoConfirmRegenerate, 500);
+            }
         } else {
             setTimeout(autoConfirmRegenerate, 500);
         }
